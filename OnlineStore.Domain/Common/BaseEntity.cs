584b2f0089eaf079a7fdeb24023395f63d863539
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-01-06 18:37:21 +0330

    create Project layer and BaseEntity and Product Model

---
+++ OnlineStore.Domain/Common/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineStore.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public long Id { get; set; }
+        public DateTime CreateAt { get; set; }= DateTime.Now;
+        public DateTime? UpdateAt { get; set; }
+        public bool IsActive { get; set; }= true;
+        public bool IsDeleted { get; set; }= false;
+    }
+}
---
+++ OnlineStore.Domain/Entities/Product.cs
@@ -0,0 +1,37 @@
+﻿using OnlineStore.Domain.Common;
+
+namespace OnlineStore.Domain.Entities
+{
+    public class Product:BaseEntity
+    {
+        public Guid ProductId { get;private set; }
+        public string Name { get;private set; }
+        public string Description { get;private set; }
+        public decimal Price { get;private set; }
+        public int Stock { get;private set; }
+        public string Slug { get;private set; }
+        public string Picture { get;private set; }
+        public string PictureAlt { get;private set; }
+        public string PictureTitle { get;private set; }
+        //public int CategoryId { get; set; }
+        //public Category Category { get; set; }
+
+        public Product(string name, string description, decimal price, int stock, string slug, string picture, string pictureAlt, string pictureTitle)
+        {
+            if(string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Product name cannot be null or empty.", nameof(name));
+            if(price < 0)
+                throw new ArgumentOutOfRangeException(nameof(price), "Price cannot be negative.");
+
+            ProductId = Guid.NewGuid();
+            Name = name;
+            Description = description;
+            Price = price;
+            Stock = stock;
+            Slug = slug;
+            Picture = picture;
+            PictureAlt = pictureAlt;
+            PictureTitle = pictureTitle;
+        }
+    }
+}
