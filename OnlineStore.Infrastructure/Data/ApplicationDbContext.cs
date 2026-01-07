9fc2c9a7385cfa88cdae1fda2ad0e7eb1f54d926
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-01-07 16:22:55 +0330

    added ApplicationDbContext for project

---
+++ OnlineStore.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineStore.Domain.Entities;
+using System.Net.Http.Headers;
+
+namespace OnlineStore.Infrastructure.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get;set; }
+        public DbSet<Category> Categories { get;set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+        }
+        
+    }
+}
