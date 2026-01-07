d6859d4e24661629667f7cd1f883797f6e0443dd
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-01-07 16:25:31 +0330

    Add GenericRepository implementation

---
+++ OnlineStore.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineStore.Domain.Common;
+using OnlineStore.Domain.Interfaces;
+using OnlineStore.Infrastructure.Data;
+
+namespace OnlineStore.Infrastructure.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        protected readonly ApplicationDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet =context.Set<T>();
+        }
+
+        public virtual async Task AddAsync(T entity)
+        {
+            await _context.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public virtual async Task DeleteAsync(T entity)
+        {
+            entity.IsDeleted = true;
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public virtual async Task<bool> ExistsAsync(long id)
+        {
+            return await _dbSet.AnyAsync(e => e.Id == id && !e.IsDeleted);
+        }
+
+        public virtual async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.AsNoTracking().Where(e => !e.IsDeleted).ToListAsync();
+        }
+
+        public virtual async Task<T?> GetByIdAsync(long id)
+        {
+            return await _dbSet.AsNoTracking().FirstOrDefaultAsync(e => e.Id == id && !e.IsDeleted);
+        }
+
+        public virtual async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
