9085d2c63d14cc2d7b1a386cd9e4d72225071f20
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-02-19 10:49:54 +0330

    update baseEntity

--- OnlineStore.Domain/Common/BaseEntity.cs
+++ OnlineStore.Domain/Common/BaseEntity.cs
@@ -3,8 +3,8 @@
     public abstract class BaseEntity
     {
         public long Id { get; set; }
-        public DateTime CreatedAt { get; set; }= DateTime.Now;
-        public DateTime? UpdateAt { get; set; }
+        public DateTime CreatedAt { get; set; }= DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
         public bool IsActive { get; set; }= true;
         public bool IsDeleted { get; set; }= false;
     }
