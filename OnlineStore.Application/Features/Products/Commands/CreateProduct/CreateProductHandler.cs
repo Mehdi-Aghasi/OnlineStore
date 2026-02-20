9b7bde0c6a1c366ddbc9bc214789bdc1ca3be809
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-02-20 10:17:02 +0330

    added createproductHandler for cqrs && MediatR Model

---
+++ OnlineStore.Application/Features/Products/Commands/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using OnlineStore.Domain.Entities;
+using OnlineStore.Domain.Interfaces;
+
+namespace OnlineStore.Application.Features.Products.Commands.CreateProduct
+{
+    public class CreateProductHandler : IRequestHandler<CreateProductCommand, long>
+    {
+        private readonly IProductRepository _repository;
+        public CreateProductHandler(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<long> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = new Product(
+                request.Name,
+                request.Description,
+                request.Price,
+                request.Slug,
+                request.CategoryId
+             );
+            await _repository.AddAsync(product);
+            return product.Id;
+        }
+    }
+}
