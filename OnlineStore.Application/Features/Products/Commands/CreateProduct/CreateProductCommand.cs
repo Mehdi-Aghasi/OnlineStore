ababa2f4122dffd347c71c3efa1ec502089a9746
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-02-20 10:15:01 +0330

    added createproductcommand

---
+++ OnlineStore.Application/Features/Products/Commands/CreateProduct/CreateProductCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace OnlineStore.Application.Features.Products.Commands.CreateProduct
+{
+    public record CreateProductCommand(
+        string Name,
+        string Description,
+        decimal Price,
+        int StockQuantity,
+        string Slug,
+        long CategoryId
+    ):IRequest<long>;
+}
