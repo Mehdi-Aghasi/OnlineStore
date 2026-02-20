5d3bdd346d5d17c6b20735ebb527e948ddc29702
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-02-20 10:15:38 +0330

    added createproductvalidation

---
+++ OnlineStore.Application/Features/Products/Commands/CreateProduct/CreateProductValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+
+namespace OnlineStore.Application.Features.Products.Commands.CreateProduct
+{
+    public class CreateProductValidator:AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Product name is required.")
+                .MaximumLength(100).WithMessage("Product name cannot exceed 100 characters.");
+            RuleFor(x => x.Description)
+                .NotEmpty().WithMessage("Product description is required.")
+                .MaximumLength(500).WithMessage("Product description cannot exceed 500 characters.");
+            RuleFor(x => x.Price)
+                .GreaterThan(0).WithMessage("Price must be greater than zero.");
+            RuleFor(x => x.StockQuantity)
+                .GreaterThanOrEqualTo(0).WithMessage("Stock quantity cannot be negative.");
+            RuleFor(x => x.Slug)
+                .NotEmpty().WithMessage("Slug is required.")
+                .MaximumLength(100).WithMessage("Slug cannot exceed 100 characters.");
+            RuleFor(x => x.CategoryId)
+                .GreaterThan(0).WithMessage("Category ID must be greater than zero.");
+        }
+
+    }
+
+}
