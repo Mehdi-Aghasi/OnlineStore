f6fb313143f048af6c5e6aa4805b988df83351d0
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-02-20 20:37:09 +0330

    added feature GetByIdProduct in application layer

---
+++ OnlineStore.Application/Features/Products/Queries/GetByIdProduct/GetByIdProductQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using OnlineStore.Application.Common.DTOs.Products;
+
+namespace OnlineStore.Application.Features.Products.Queries.GetByIdProduct
+{
+    public record GetByIdProductQuery(long Id):IRequest<ProductDto>;
+
+
+}
