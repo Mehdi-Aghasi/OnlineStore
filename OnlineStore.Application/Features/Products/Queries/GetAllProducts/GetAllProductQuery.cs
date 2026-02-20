9c12394775a97fe5a02795e3a1d8ef71e61b2368
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-02-20 10:18:24 +0330

    adde GetAllProduct for cqrs &&MediatR Madel

---
+++ OnlineStore.Application/Features/Products/Queries/GetAllProducts/GetAllProductQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using OnlineStore.Application.Common.DTOs.Products;
+
+namespace OnlineStore.Application.Features.Products.Queries.GetAllProducts
+{
+    public class GetAllProductQuery : IRequest<IEnumerable<ProductDto>> { }
+
+}
