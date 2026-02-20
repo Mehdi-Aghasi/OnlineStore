1e17b65c4e63f8d0a9c9cbc6b9061c3759253d26
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-02-20 20:12:28 +0330

    added deleteproductvalidator

---
+++ OnlineStore.Application/Features/Products/Commands/DeleteProduct/DeleteProductValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace OnlineStore.Application.Features.Products.Commands.DeleteProduct
+{
+    public class DeleteProductValidator:AbstractValidator<DeleteProductCommand>
+    {
+        public DeleteProductValidator()
+        {
+            RuleFor(x => x.Id)
+                .GreaterThan(0).WithMessage("Product ID must be greater than 0.")
+        }
+    }
+}
