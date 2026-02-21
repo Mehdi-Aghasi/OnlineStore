a1c787cc5eeafbd337a8f5da311f8315d0288bd0
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-02-21 14:54:14 +0330

    added featur UpdateCategory in a way cqrs and mediatr

---
+++ OnlineStore.Application/Features/Categories/Commands/UpdateCategory/UpdateCategoryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using OnlineStore.Application.Common.DTOs.Categories;
+using OnlineStore.Domain.Entities;
+using OnlineStore.Domain.Interfaces;
+
+namespace OnlineStore.Application.Features.Categories.Commands.UpdateCategory
+{
+    public class UpdateCategoryHandler:IRequestHandler<UpdateCategoryCommand, CategoryDto>
+    {
+        private readonly ICategoryRepository _repository;
+        private readonly IMapper _mapper;
+        public UpdateCategoryHandler(ICategoryRepository repository,IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+        public async Task<CategoryDto> Handle(UpdateCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var category = await _repository.FindByIdAsync(request.Id);
+            if (category == null)
+            {
+                throw new KeyNotFoundException($"Category with ID {request.Id} not found.");
+            }
+            category.Update(request.Name, request.Description, request.Slug);
+            await _repository.UpdateAsync(category);
+            return _mapper.Map<CategoryDto>(category);
+        }
+    }
+}
---
+++ OnlineStore.Application/Features/Categories/Commands/UpdateCategory/UpdateCategoryValodator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace OnlineStore.Application.Features.Categories.Commands.UpdateCategory
+{
+    public class UpdateCategoryValodator:AbstractValidator<UpdateCategoryCommand>
+    {
+        public UpdateCategoryValodator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Category name is required.")
+                .MaximumLength(100).WithMessage("Category name cannot exceed 100 characters.");
+            RuleFor(x => x.Description)
+                .MaximumLength(500).WithMessage("Category description cannot exceed 500 characters.");
+            RuleFor(x => x.Slug)
+                .NotEmpty().WithMessage("Category slug is required.")
+                .MaximumLength(100).WithMessage("Category slug cannot exceed 100 characters.");
+        }
+    }
+}
