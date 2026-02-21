a16675b2e6fdba4f9bcd5ca471f44d8fd790f505
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-02-21 14:57:39 +0330

    added category profile in project

---
+++ OnlineStore.Application/Common/Mappings/CategoryProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using OnlineStore.Application.Common.DTOs.Categories;
+using OnlineStore.Domain.Entities;
+
+namespace OnlineStore.Application.Common.Mappings
+{
+    public class CategoryProfile:Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<Category,CategoryDto>();
+        }
+    }
+}
