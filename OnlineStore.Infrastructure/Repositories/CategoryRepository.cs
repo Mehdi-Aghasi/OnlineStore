81d073f60321387cd194a0c4329b9a3b8ac38b26
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-02-21 14:58:43 +0330

    added inteface Icategoryrepository

---
+++ OnlineStore.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using OnlineStore.Domain.Entities;
+using OnlineStore.Domain.Interfaces;
+using OnlineStore.Infrastructure.Data;
+
+namespace OnlineStore.Infrastructure.Repositories
+{
+    public class CategoryRepository:GenericRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(ApplicationDbContext context) : base(context) { }
+    }
+}
