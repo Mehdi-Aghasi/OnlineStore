02d56c3e2262f9310e459ad10ed6798704206997
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-02-21 14:55:12 +0330

    added featur GetAllCategory in project

---
+++ OnlineStore.Application/Features/Categories/Queries/GetAllCategory/GetAllCategoryHandler.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using MediatR;
+using OnlineStore.Application.Common.DTOs.Categories;
+using OnlineStore.Domain.Interfaces;
+
+namespace OnlineStore.Application.Features.Categories.Queries.GetAllCategory
+{
+    public class GetAllCategoryHandler : IRequestHandler<GetAllCategoryQuery, IEnumerable<CategoryDto>>
+    {
+        private readonly ICategoryRepository _repository;
+        private readonly IMapper _mapper;
+        public GetAllCategoryHandler(ICategoryRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+        public async Task<IEnumerable<CategoryDto>> Handle(GetAllCategoryQuery request, CancellationToken cancellationToken)
+        {
+            var categories = await _repository.GetAllAsync();
+            return _mapper.Map<IEnumerable<CategoryDto>>(categories);
+        }
+    }
+}
