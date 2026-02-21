6ec39b748cd138851963ed8f4338ef728652210c
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-02-21 14:55:46 +0330

    added feature getbyidcategory in project

---
+++ OnlineStore.Application/Features/Categories/Queries/GetByIdCategory/GetByIdCategoryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MediatR;
+using OnlineStore.Application.Common.DTOs.Categories;
+using OnlineStore.Domain.Interfaces;
+
+namespace OnlineStore.Application.Features.Categories.Queries.GetByIdCategory
+{
+    public class GetByIdCategoryHandler : IRequestHandler<GetByIdCategoryQuery, CategoryDto>
+    {
+        private readonly ICategoryRepository _repository;
+        private readonly IMapper _mapper;
+        public GetByIdCategoryHandler(ICategoryRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+        public async Task<CategoryDto> Handle(GetByIdCategoryQuery request, CancellationToken cancellationToken)
+        {
+            var category = await _repository.GetByIdAsync(request.Id);
+            if (category == null)
+            {
+                throw new KeyNotFoundException($"Category with ID {request.Id} not found.");
+            }
+            return _mapper.Map<CategoryDto>(category);
+        }
+    }
+}
---
+++ OnlineStore.Application/Features/Categories/Queries/GetByIdCategory/GetByIdCategoryQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using OnlineStore.Application.Common.DTOs.Categories;
+
+namespace OnlineStore.Application.Features.Categories.Queries.GetByIdCategory
+{
+    public record GetByIdCategoryQuery(
+        long Id
+    ) : IRequest<CategoryDto>;
+}
