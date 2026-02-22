31a0bd65d6ec4437b69d48dce03768319dc79da5
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-02-22 21:22:02 +0330

    add orders Models for Db

--- OnlineStore.Infrastructure/Data/ApplicationDbContext.cs
+++ OnlineStore.Infrastructure/Data/ApplicationDbContext.cs
@@ -12,6 +12,8 @@ namespace OnlineStore.Infrastructure.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Cart> Carts { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
