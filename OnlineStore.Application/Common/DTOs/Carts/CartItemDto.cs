3241be80a9ea7066f3d6cecb264e2c36c0acdce9
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-02-22 10:02:26 +0330

    add carts Dtos in applicarion layer for cqrs model

---
+++ OnlineStore.Application/Common/DTOs/Carts/CartItemDto.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineStore.Application.Common.DTOs.Carts
+{
+    public record CartItemDto(
+        long Id,
+        long ProductId,
+        string ProductName,
+        int Quantity,
+        decimal Price
+        );
+}
