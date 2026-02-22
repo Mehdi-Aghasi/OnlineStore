5250e07c975a20c1f5d1cb8741dc86760cb8b4a6
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-02-22 10:04:15 +0330

    add feature RemoveFromCart

---
+++ OnlineStore.Application/Features/Carts/Commands/RemoveFromCart/RemoveFromCartHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using OnlineStore.Application.Features.Carts.Commands.RemoveFromCart;
+using OnlineStore.Domain.Interfaces;
+
+public class RemoveFromCartHandler : IRequestHandler<RemoveFromCartCommand>
+{
+    private readonly ICartItemRepository _repository;
+
+    public RemoveFromCartHandler(ICartItemRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task Handle(RemoveFromCartCommand request, CancellationToken cancellationToken)
+    {
+        var cartItem = await _repository.FindByIdAsync(request.CartItemId);
+        if (cartItem == null)
+        {
+            throw new KeyNotFoundException($"CartItem with ID {request.CartItemId} not found.");
+        }
+        await _repository.DeleteAsync(cartItem);
+    }
+}
