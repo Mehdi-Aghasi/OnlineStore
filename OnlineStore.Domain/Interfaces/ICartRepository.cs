3e7bb53da1759c86d439031176ac5d4bd84ad329
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-02-22 09:57:59 +0330

    add Interface repository for Cart Entity

---
+++ OnlineStore.Domain/Interfaces/ICartRepository.cs
@@ -0,0 +1,9 @@
+﻿using OnlineStore.Domain.Entities;
+
+namespace OnlineStore.Domain.Interfaces
+{
+    public interface ICartRepository:IGenericRepository<Cart>
+    {
+        Task<Cart?> GetCartByUserIdAsync(string userId);
+    }
+}
