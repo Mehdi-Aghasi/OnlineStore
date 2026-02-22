6196b8c44dea97b5854b3da5ab1d398f9bbda2dc
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-02-22 10:01:27 +0330

    implement Interface repository cart in infras layer

---
+++ OnlineStore.Infrastructure/Repositories/CartRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineStore.Domain.Entities;
+using OnlineStore.Domain.Interfaces;
+using OnlineStore.Infrastructure.Data;
+
+namespace OnlineStore.Infrastructure.Repositories
+{
+    public class CartRepository : GenericRepository<Cart>, ICartRepository
+    {
+        public CartRepository(ApplicationDbContext context) : base(context) { }
+        public async Task<Cart?> GetCartByUserIdAsync(string userId)
+        {
+            return await _dbSet.
+                  Include(c => c.CartItems)
+                  .ThenInclude(p => p.Product)
+                  .FirstOrDefaultAsync(c => c.UserId == userId && !c.IsDeleted);
+        }
+    }
+}
