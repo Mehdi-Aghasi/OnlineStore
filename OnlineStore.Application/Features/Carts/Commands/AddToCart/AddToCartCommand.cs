0762ad10fd48d3be7b45f9f031a8c4e67f51efa7
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-02-22 10:03:47 +0330

    add feature AddTocart

---
+++ OnlineStore.Application/Features/Carts/Commands/AddToCart/AddToCartCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace OnlineStore.Application.Features.Carts.Commands.AddToCart
+{
+    public record AddToCartCommand(
+        string UserId,
+        long ProductId,
+        int Quantity,
+        decimal Price
+    ) : IRequest;
+}
---
+++ OnlineStore.Application/Features/Carts/Commands/AddToCart/AddToCartValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace OnlineStore.Application.Features.Carts.Commands.AddToCart
+{
+    public class AddToCartValidator:AbstractValidator<AddToCartCommand>
+    {
+        public AddToCartValidator()
+        {
+            RuleFor(x => x.ProductId)
+                .GreaterThan(0).WithMessage("Product ID must be greater than 0.");
+            RuleFor(x => x.Quantity)
+                .GreaterThan(0).WithMessage("Quantity must be greater than 0.");
+            RuleFor(x => x.Price)
+                .GreaterThan(0).WithMessage("Price must be greater than 0.");
+        }
+    }
+}
