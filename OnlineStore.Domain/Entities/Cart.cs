94c5c1158a30b7eb33db494f14d34e32167c66e8
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-02-22 09:57:22 +0330

    add Entity model cart and cartItem for project and add relation cart and product

---
+++ OnlineStore.Domain/Entities/Cart.cs
@@ -0,0 +1,32 @@
+﻿using OnlineStore.Domain.Common;
+
+namespace OnlineStore.Domain.Entities
+{
+    public class Cart:BaseEntity
+    {
+        public string UserId { get;private set; }
+        public ICollection<CartItem> CartItems { get;private set; }
+
+        public Cart() { }
+        public Cart(string userId)
+        {
+            if(string.IsNullOrWhiteSpace(userId))
+                throw new ArgumentException("User ID cannot be null or empty.", nameof(userId));
+            UserId = userId;
+            CartItems = new List<CartItem>();
+        }
+
+        public void AddCartItem(CartItem cartItem)
+        {
+            if (cartItem == null)
+                throw new ArgumentNullException(nameof(cartItem), "Cart item cannot be null.");
+            CartItems.Add(cartItem);
+        }
+            public void RemoveCartItem(CartItem cartItem)
+            {
+                if (cartItem == null)
+                    throw new ArgumentNullException(nameof(cartItem), "Cart item cannot be null.");
+                CartItems.Remove(cartItem);
+        }
+    }
+}
---
+++ OnlineStore.Domain/Entities/CartItem.cs
@@ -0,0 +1,28 @@
+﻿using OnlineStore.Domain.Common;
+
+namespace OnlineStore.Domain.Entities
+{
+    public class CartItem:BaseEntity
+    {
+        public long CartId { get;private set; }
+        public long ProductId { get;private set; }
+        public int Quantity { get;private set; }
+        public decimal Price { get;private set; }
+        public Cart Cart { get;private set; }
+        public Product Product { get;private set; }
+
+        public CartItem(long id,long productId,int quantity,decimal price)
+        {
+            if(id <= 0)
+                throw new ArgumentException("Cart ID must be greater than zero.", nameof(id));
+            if(productId <= 0)
+                throw new ArgumentException("Product ID must be greater than zero.", nameof(productId));
+            if(quantity <= 0)
+                throw new ArgumentException("Quantity must be greater than zero.", nameof(quantity));
+            CartId = id;
+            ProductId = productId;
+            Quantity = quantity;
+            Price = price;
+        }
+    }
+}
