703c7df2919686282d8e11d76fecc8f536bddedd
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-02-22 10:02:52 +0330

    add cartProfile

---
+++ OnlineStore.Application/Common/Mappings/CartProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using OnlineStore.Application.Common.DTOs.Carts;
+using OnlineStore.Domain.Entities;
+
+namespace OnlineStore.Application.Common.Mappings
+{
+    public class CartProfile:Profile
+    {
+        public CartProfile()
+        {
+            CreateMap<Cart,CartDto>()
+                .ForMember(dest => dest.Items, opt => opt.MapFrom(src => src.CartItems));
+            CreateMap<CartItem,CartItemDto>()
+                .ForMember(dest=>dest.ProductName,opt=>opt.MapFrom(src=>src.Product.Name));
+        }
+    }
+}
