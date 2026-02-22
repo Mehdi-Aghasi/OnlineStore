fb56f5806a15cd5e9a4474703ec9bd4b5a18f5a4
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-02-22 09:59:56 +0330

    add Configuration for carts Entity  and update configuration product add relation cart , porduct

---
+++ OnlineStore.Infrastructure/Configurations/CartItemConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineStore.Domain.Entities;
+
+namespace OnlineStore.Infrastructure.Configurations
+{
+    public class CartItemConfiguration : IEntityTypeConfiguration<CartItem>
+    {
+        public void Configure(EntityTypeBuilder<CartItem> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.CartId).IsRequired();
+            builder.Property(x => x.ProductId).IsRequired();
+            builder.Property(x => x.Quantity).IsRequired();
+            builder.HasQueryFilter(x => !x.IsDeleted);
+            builder.HasOne(p => p.Product)
+                .WithMany(c=>c.CartItems)
+                .HasForeignKey(c => c.ProductId)
+                .OnDelete(DeleteBehavior.Restrict);
+            builder.Property(x => x.Price).IsRequired().HasColumnType("decimal(18,2)");
+            builder.HasOne(c => c.Cart)
+                .WithMany(c => c.CartItems)
+                .HasForeignKey(c => c.CartId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
