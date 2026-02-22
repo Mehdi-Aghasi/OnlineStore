a6a9b0ab8b79cc386b91437dd487978a098b3d3c
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-02-22 21:30:39 +0330

    add feature UpdateorderStatus

---
+++ OnlineStore.Application/Features/Orders/Commands/UpdateOrderStatus/UpdateOrderStatusCammand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using OnlineStore.Domain.Common.Enums;
+
+namespace OnlineStore.Application.Features.Orders.Commands.UpdateOrderStatus
+{
+    public record UpdateOrderStatusCommand(
+    long OrderId,
+    OrderStatus NewStatus
+) : IRequest;
+}
---
+++ OnlineStore.Application/Features/Orders/Commands/UpdateOrderStatus/UpdateOrderStatusHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using OnlineStore.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineStore.Application.Features.Orders.Commands.UpdateOrderStatus
+{
+    public class UpdateOrderStatusHandler : IRequestHandler<UpdateOrderStatusCommand>
+    {
+        private readonly IOrderRepository _repository;
+
+        public UpdateOrderStatusHandler(IOrderRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateOrderStatusCommand request, CancellationToken cancellationToken)
+        {
+            var order = await _repository.FindByIdAsync(request.OrderId);
+            if (order == null)
+                throw new KeyNotFoundException($"Order with ID {request.OrderId} not found.");
+
+            order.UpdateStatus(request.NewStatus);
+            await _repository.UpdateAsync(order);
+        }
+    }
+}
