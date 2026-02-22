06bd42ca6db46d14a1819e598e106731c63124f0
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-02-22 10:04:58 +0330

    add feature GetByUserId

---
+++ OnlineStore.Application/Features/Carts/Queries/GetCartByUserId/GetCartByUserIdHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MediatR;
+using OnlineStore.Application.Common.DTOs.Carts;
+using OnlineStore.Domain.Interfaces;
+
+namespace OnlineStore.Application.Features.Carts.Queries.GetCartByUserId
+{
+    public class GetCartByUserIdHandler : IRequestHandler<GetCartByUserIdQuery, CartDto>
+    {
+        private readonly ICartRepository _cartRepository;
+        private readonly IMapper _mapper;
+        public GetCartByUserIdHandler(ICartRepository cartRepository,IMapper mapper)
+        {
+            _cartRepository = cartRepository;
+            _mapper = mapper;
+        }
+        public async Task<CartDto> Handle(GetCartByUserIdQuery request, CancellationToken cancellationToken)
+        {
+            var cart = await _cartRepository.GetCartByUserIdAsync(request.UserId);
+            if (cart == null)
+            {
+                throw new KeyNotFoundException($"Cart for user {request.UserId} not found.");
+            }
+            return _mapper.Map<CartDto>(cart);
+        }
+    }
+}
