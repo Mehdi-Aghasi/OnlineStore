169b3369d3a89431f2c98dede9efb19bce70267e
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-02-22 21:06:03 +0330

    add Order, orderItem Model in Project

---
+++ OnlineStore.Domain/Entities/OrderItem.cs
@@ -0,0 +1,30 @@
+﻿using OnlineStore.Domain.Common;
+
+namespace OnlineStore.Domain.Entities
+{
+    public class OrderItem: BaseEntity
+    {
+        public long OrderId { get; private set; }
+        public long ProductId { get; private set; }
+        public int Quantity { get; private set; }
+        public decimal Price { get; private set; }
+        public Order Order { get; private set; }
+        public Product Product { get; private set; }
+
+
+        public OrderItem(long productId, long orderId, int quantity, decimal price)
+        {
+            if (quantity <= 0)
+                throw new ArgumentOutOfRangeException(nameof(quantity), "Quantity must be greater than zero.");
+            if (price < 0)
+                throw new ArgumentOutOfRangeException(nameof(price), "Price cannot be negative.");
+            if (productId <= 0)
+                throw new ArgumentOutOfRangeException(nameof(productId), "Product ID must be greater than zero.");
+            ProductId = productId;
+            OrderId = orderId;
+            Quantity = quantity;
+            Price = price;
+        }
+
+    }
+}
