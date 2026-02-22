addaaa7e6f76697218ad806410e2321d662aa725
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-02-22 21:28:54 +0330

    add orderProfile

---
+++ OnlineStore.Application/Common/Mappings/OrderProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using OnlineStore.Application.Common.DTOs.Orders;
+using OnlineStore.Domain.Entities;
+
+namespace OnlineStore.Application.Common.Mappings
+{
+    public class OrderProfile : Profile
+    {
+        public OrderProfile()
+        {
+            CreateMap<Order, OrderDto>()
+                .ForMember(dest => dest.OrderStatus,
+                    opt => opt.MapFrom(src => src.OrderStatus.ToString()));
+
+            CreateMap<OrderItem, OrderItemDto>()
+                .ForMember(dest => dest.ProductName,
+                    opt => opt.MapFrom(src => src.Product.Name));
+        }
+    }
+}
