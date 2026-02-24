3c006ec5027b5c4ccc5902d19e52c0c1c54be083
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-02-24 15:59:47 +0330

    Add identity service interface and ApplicationUser entity
    Added IIdentityService interface for user auth operations. Introduced ApplicationUser entity with first and last name fields. Set up DependencyInjection class. Updated project references: downgraded AutoMapper, added AutoMapper.Extensions.Microsoft.DependencyInjection, removed FluentValidation DI extension, and added Microsoft.Extensions.Identity.Stores to domain project.

---
+++ OnlineStore.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace OnlineStore.Domain.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
