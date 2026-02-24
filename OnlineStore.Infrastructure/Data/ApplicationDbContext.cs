92e7db69eaeae9e18ad517eb1ee441de1f75f4d4
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2026-02-24 16:00:33 +0330

    Integrate ASP.NET Core Identity and add IdentityService
    Updated ApplicationDbContext to use IdentityDbContext<ApplicationUser> for user management. Added IdentityService for authentication and registration with JWT support. Updated project references to include Identity package and OnlineStore.Application.

--- OnlineStore.Infrastructure/Data/ApplicationDbContext.cs
+++ OnlineStore.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using OnlineStore.Domain.Entities;
 using OnlineStore.Infrastructure.Configurations;
 
 namespace OnlineStore.Infrastructure.Data
 {
-    public class ApplicationDbContext :DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
